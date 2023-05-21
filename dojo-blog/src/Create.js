e69ca38a30d0fc16d65bb6901cd587cb0548d825
melanasia <melanie.ali54@gmail.com>
2023-05-21 10:39:55 -0700

    moved project to new repo

---
+++ dojo-blog/src/Create.js
@@ -0,0 +1,26 @@
+const Create = () => {
+    return ( 
+        <div className="create">
+            <h2>Add a new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input
+                  type="text"
+                  required
+                />
+                <label>Blog body:</label>
+                <textarea
+                  required
+                ></textarea>
+                <label>Blog author:</label>
+                <select>
+                  <option value="mario">mario</option> 
+                  <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add blog</button>
+            </form>
+        </div>
+     );
+}
+ 
+export default Create;
