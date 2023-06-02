cea4192e9fa748a4ca974831a9fb72b0eea55ec7
melanasia <melanie.ali54@gmail.com>
2023-06-01 17:36:46 -0700

    add forms

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario')
+
     return ( 
         <div className="create">
             <h2>Add a new blog</h2>
@@ -7,17 +13,27 @@ const Create = () => {
                 <input
                   type="text"
                   required
+                  value={title}
+                  onChange={(ev) => setTitle(ev.target.value)}
                 />
                 <label>Blog body:</label>
                 <textarea
                   required
+                  value={body}
+                  onChange={(ev) => setBody(ev.target.value)}
                 ></textarea>
                 <label>Blog author:</label>
-                <select>
+                <select
+                  value={author}
+                  onChange={(ev) => setAuthor(ev.target.value)}
+                >
                   <option value="mario">mario</option> 
                   <option value="yoshi">yoshi</option>
                 </select>
                 <button>Add blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
             </form>
         </div>
      );
